0e4055e5a4597740fdd4f8a406fd31ba50112271
Gropp <fgropp@gmail.com>
2022-08-06 22:32:44 -0300

    configuracao

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry:'./teste.js',
+    entry:'./src/index.js',
     output: {
-        path: path.resolve(__dirname + "/webpack", 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 };
